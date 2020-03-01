6deb4e129fa3f7cae4a8dfd6753d65044395e3ff
Rémi Duret <rd.duret@hotmail.fr>
2020-02-29 19:45:44 -0500

    Affichage de la pensée lorsqu'on clic sur le bouton pour #1

--- client/src/VueInspirationVisuelle.java
+++ client/src/VueInspirationVisuelle.java
@@ -5,8 +5,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import modele.Pensee;
 
 public class VueInspirationVisuelle extends Application {
 	public VueInspirationVisuelle()
@@ -25,6 +27,14 @@ public class VueInspirationVisuelle extends Application {
 		scene = new Scene(racine, 800, 600);
 		stade.setScene(scene);
 		stade.show();
+	}
+	public void afficherPensee(Pensee pensee)
+	{
+    	System.out.println("afficherPensee( )");
+
+		TextArea zonePensee = (TextArea)scene.lookup("#zonepensee");
+		//zonePensee.setText("coucou");		
+		zonePensee.setText(pensee.getMessage() + " (" + pensee.getAuteur() + ")");
 	}	
 	
 	
